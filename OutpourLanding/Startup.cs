85e3cf1203864f4c6265d55f43082236ec72c3ea
drmarshall <cptcecil@gmail.com>
2014-08-26 22:12:52 -0400

    Initial Commit

---
+++ OutpourLanding/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OutpourLanding.Startup))]
+namespace OutpourLanding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
